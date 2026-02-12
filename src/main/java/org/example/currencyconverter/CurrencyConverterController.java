381f7cf49b98fa098a1a55084f3fef029e3369ab
SebastianSchwingenschuh <s.schwingenschuh@students.htl-leonding.ac.at>
2026-02-12 11:05:57 +0100

    fxml

--- src/main/java/org/example/currencyconverter/CurrencyConverterController.java
+++ src/main/java/org/example/currencyconverter/CurrencyConverterController.java
@@ -1,19 +1,36 @@
 package org.example.currencyconverter;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CurrencyConverterController {
     @FXML
+    private TextField amountToConvert
+    @FXML
+    @FXML
+    @FXML
+    
+    @FXML
     private TextField amountToConvert;
-//    @FXML
-//    private Label welcomeText;
-//
-//    @FXML
-//    protected void onHelloButtonClick() {
-//        welcomeText.setText("Welcome to JavaFX Application!");
-//    }
+
+    public void handleConversionChange() {
+        
+    }
+
+
+    public void handleReset() {
+        
+    }
+
+    public void handleConvert() {
+    }
     
-    
+    @FXML
+    private void initialize(){
+        handleReset();
+        desired
+        
+    }
 }
