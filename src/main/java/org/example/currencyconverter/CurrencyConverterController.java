7aeae22c02a0fdc65386ffbd7964a460eb6b21c9
SebastianSchwingenschuh <s.schwingenschuh@students.htl-leonding.ac.at>
2026-02-12 11:09:13 +0100

    fields im controller

--- src/main/java/org/example/currencyconverter/CurrencyConverterController.java
+++ src/main/java/org/example/currencyconverter/CurrencyConverterController.java
@@ -2,18 +2,23 @@ package org.example.currencyconverter;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CurrencyConverterController {
     @FXML
-    private TextField amountToConvert
-    @FXML
-    @FXML
-    @FXML
+    private TextField amountToConvert;
     
     @FXML
-    private TextField amountToConvert;
+    private TextField convertedAmount;
+    
+    @FXML
+    private ComboBox<String> desiredConversion;
+    
+    @FXML
+    private Canvas canvas;
 
     public void handleConversionChange() {
         
